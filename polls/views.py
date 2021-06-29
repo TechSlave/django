afdb2bd2f6241d547fd38f614f18b0163b89aa9b
TechSlave <itamarportela@gmail.com>
2021-06-29 18:08:11 -0300

    .

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Polls Index")
+
+def detail(request, question_id):
+    return HttpResponse("You're loogink at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
